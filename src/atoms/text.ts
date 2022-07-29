979a468355f253e483e6638cd998af5584020f60
FabioRocha231 <fabioharoldo1@gmail.com>
2022-07-29 13:34:47 -0300

    feat: implemented atoms

---
+++ src/atoms/text.ts
@@ -0,0 +1,8 @@
+import { Theme } from '@/themes'
+import { createText } from '@shopify/restyle'
+
+
+const Text = createText<Theme>()
+export type TextProps = React.ComponentProps<typeof Text>
+
+export default Text
