07657965ff117607c1c77198fb5e76eee8fa26bb
FabioRocha231 <fabioharoldo1@gmail.com>
2022-07-29 23:25:04 -0300

    feat: atoms pressable and touchable and icon component

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/atoms";
 import HeaderBar from "@/components/header-bar";
+import FeatherIcon from "@/components/icon";
 import NoteList from "@/components/note-list";
 import React from "react";
 
@@ -7,7 +8,10 @@ export default function MainScreen() {
   return (
     <Container justifyContent="center" alignItems="center">
       <NoteList />
-      <HeaderBar />
+      <HeaderBar>
+        <FeatherIcon name="menu" size={22} />
+        <FeatherIcon name="more-vertical" size={22} />
+      </HeaderBar>
     </Container>
   )
 }
