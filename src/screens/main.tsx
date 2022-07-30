9a018cb8a4e1256c63e258c11c8c3afffbb60285
FabioRocha231 <fabioharoldo1@gmail.com>
2022-07-29 23:59:15 -0300

    fix: move Feather config to the correct file

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@/atoms";
+import { Box, Container, Text, TouchableOpacity } from "@/atoms";
 import HeaderBar from "@/components/header-bar";
 import FeatherIcon from "@/components/icon";
 import NoteList from "@/components/note-list";
@@ -9,8 +9,15 @@ export default function MainScreen() {
     <Container justifyContent="center" alignItems="center">
       <NoteList />
       <HeaderBar>
-        <FeatherIcon name="menu" size={22} />
-        <FeatherIcon name="more-vertical" size={22} />
+        <TouchableOpacity m="xs" p="xs" rippleBorderless>
+          <FeatherIcon name="menu" size={22} />
+        </TouchableOpacity>
+        <Box flex={1} alignItems="center">
+          <Text fontWeight="bold">All Notes</Text>
+        </Box>
+        <TouchableOpacity m="xs" p="xs" rippleBorderless>
+          <FeatherIcon name="more-vertical" size={22} />
+        </TouchableOpacity>
       </HeaderBar>
     </Container>
   )
